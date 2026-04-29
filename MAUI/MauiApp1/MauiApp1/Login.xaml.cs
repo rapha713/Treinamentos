178978fc8c23e2716aa2b484d2d9f37a8cbe9d2c
raphaelapice <raphael.oliveira@administraconsorcio.com.br>
2026-04-29 12:36:12 -0300

    ajustando Login

--- MAUI/MauiApp1/MauiApp1/Login.xaml.cs
+++ MAUI/MauiApp1/MauiApp1/Login.xaml.cs
@@ -14,11 +14,16 @@ public partial class Login : ContentPage
         bool usuarioValido = user == "admin";
         bool senhaValida = senha == "123";
 
+        await LoginButton.ScaleToAsync(0.95, 100);
+
         if (string.IsNullOrWhiteSpace(user) || string.IsNullOrWhiteSpace(senha))
         {
             await DisplayAlertAsync("Erro", "Preencha todos os campos", "OK");
             UserEntry.BackgroundColor = Colors.Red;
             UserEntry.TextColor = Colors.White;
+            PasswordEntry.BackgroundColor = Colors.Red;
+            PasswordEntry.TextColor = Colors.White;
+            await LoginButton.ScaleToAsync(1, 100);
             return;
         }
         else
@@ -42,7 +47,7 @@ public partial class Login : ContentPage
                 }
                 else
                 {
-                    if (usuarioValido)
+                    if (!usuarioValido)
                     {
                         UserEntry.BackgroundColor = Colors.Red;
                         UserEntry.PlaceholderColor = Colors.White;
@@ -51,9 +56,10 @@ public partial class Login : ContentPage
                     {
                         UserEntry.BackgroundColor = Colors.Transparent;
                         UserEntry.PlaceholderColor = Colors.DarkGray;
+                        UserEntry.TextColor = Colors.Black;
                     }
 
-                    if (senhaValida)
+                    if (!senhaValida)
                     {
                         PasswordEntry.BackgroundColor = Colors.Red;
                         PasswordEntry.PlaceholderColor = Colors.White;
@@ -62,50 +68,62 @@ public partial class Login : ContentPage
                     {
                         PasswordEntry.BackgroundColor = Colors.Transparent;
                         PasswordEntry.PlaceholderColor = Colors.DarkGray;
+                        PasswordEntry.TextColor = Colors.Black;
                     }
 
                     await DisplayAlertAsync("Erro", "Credenciais inválidas", "OK");
+                    await LoginButton.ScaleToAsync(1, 100);
                 }
             }
             finally
             {
+                await LoginButton.ScaleToAsync(1, 100);
                 await Loading(2);
             }
         }
     }
 
     //CLICAR PARA APARECER
-    //private bool isPasswordVisible = false;
+    private bool isPasswordVisible = false;
 
-    //private async void OnTogglePasswordClicked(object sender, EventArgs e)
-    //{
-    //    isPasswordVisible = !isPasswordVisible;
-    //    PasswordEntry.IsPassword = !isPasswordVisible;
-    //    TogglePasswordButton.Text = isPasswordVisible ? "🙈" : "👁";
+    private async void OnTogglePasswordClicked(object sender, EventArgs e)
+    {
+        isPasswordVisible = !isPasswordVisible;
 
-    //    if (isPasswordVisible)
-    //    {
-    //        await Task.Delay(3000);
+        PasswordEntry.IsPassword = !isPasswordVisible;
 
-    //        if (isPasswordVisible)
-    //        {
-    //            isPasswordVisible = false;
-    //            PasswordEntry.IsPassword = true;
-    //            TogglePasswordButton.Text = "👁";
-    //        }
-    //    }
-    //}
+        TogglePasswordButton.Source = isPasswordVisible
+            ? "eyen.png"
+            : "eye.png";
+
+        await TogglePasswordButton.ScaleToAsync(0.8, 100);
+        await TogglePasswordButton.ScaleToAsync(1, 100);
+    }
 
     //SEGURAR PARA APARECER
-    private void OnPressed(object sender, EventArgs e)
-    {
-        PasswordEntry.IsPassword = false;
-    }
+    //private void OnPressed(object sender, EventArgs e)
+    //{
+    //    PasswordEntry.IsPassword = false;
+    //    EyeIcon.Source = "eyen.png";
+    //    EyeContainer.Scale = 0.8;
+    //}
 
-    private void OnReleased(object sender, EventArgs e)
-    {
-        PasswordEntry.IsPassword = true;
-    }
+    //private void OnReleased(object sender, EventArgs e)
+    //{
+    //    PasswordEntry.IsPassword = true;
+    //    EyeIcon.Source = "eye.png";
+    //    EyeContainer.Scale = 1;
+    //}
+
+    //private void OnHoverEnter(object sender, EventArgs e)
+    //{
+    //    EyeContainer.BackgroundColor = Colors.Azure;
+    //}
+
+    //private void OnHoverExit(object sender, EventArgs e)
+    //{
+    //    EyeContainer.BackgroundColor = Colors.Transparent;
+    //}
 
     async Task<bool> FakeLogin(string user, string senha)
     {
