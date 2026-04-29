0cafd6ffd54f7746dac2ed9ee457dde747f9fd98
raphaelapice <raphael.oliveira@administraconsorcio.com.br>
2026-04-29 08:55:18 -0300

    MAUI

---
+++ MAUI/MauiApp1/MauiApp1/Login.xaml.cs
@@ -0,0 +1,132 @@
+namespace MauiApp1;
+
+public partial class Login : ContentPage
+{
+    public Login()
+	{
+		InitializeComponent();
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        string user = UserEntry.Text;
+        string senha = PasswordEntry.Text;
+        bool usuarioValido = user == "admin";
+        bool senhaValida = senha == "123";
+
+        if (string.IsNullOrWhiteSpace(user) || string.IsNullOrWhiteSpace(senha))
+        {
+            await DisplayAlertAsync("Erro", "Preencha todos os campos", "OK");
+            UserEntry.BackgroundColor = Colors.Red;
+            UserEntry.TextColor = Colors.White;
+            return;
+        }
+        else
+        {
+            try
+            {
+                await Loading(1);
+
+                bool sucesso = await FakeLogin(user, senha);
+
+                if (sucesso)
+                {
+                    await DisplayAlertAsync("Sucesso", $"Login realizado como {user}!", "OK");
+                    UserEntry.Text = "";
+                    PasswordEntry.Text = "";
+                    UserEntry.BackgroundColor = Colors.Transparent;
+                    UserEntry.PlaceholderColor = Colors.DarkGray;
+                    PasswordEntry.BackgroundColor = Colors.Transparent;
+                    PasswordEntry.PlaceholderColor = Colors.DarkGray;
+                    await Navigation.PushAsync(new MainPage());
+                }
+                else
+                {
+                    if (usuarioValido)
+                    {
+                        UserEntry.BackgroundColor = Colors.Red;
+                        UserEntry.PlaceholderColor = Colors.White;
+                    }
+                    else
+                    {
+                        UserEntry.BackgroundColor = Colors.Transparent;
+                        UserEntry.PlaceholderColor = Colors.DarkGray;
+                    }
+
+                    if (senhaValida)
+                    {
+                        PasswordEntry.BackgroundColor = Colors.Red;
+                        PasswordEntry.PlaceholderColor = Colors.White;
+                    }
+                    else
+                    {
+                        PasswordEntry.BackgroundColor = Colors.Transparent;
+                        PasswordEntry.PlaceholderColor = Colors.DarkGray;
+                    }
+
+                    await DisplayAlertAsync("Erro", "Credenciais inválidas", "OK");
+                }
+            }
+            finally
+            {
+                await Loading(2);
+            }
+        }
+    }
+
+    //CLICAR PARA APARECER
+    //private bool isPasswordVisible = false;
+
+    //private async void OnTogglePasswordClicked(object sender, EventArgs e)
+    //{
+    //    isPasswordVisible = !isPasswordVisible;
+    //    PasswordEntry.IsPassword = !isPasswordVisible;
+    //    TogglePasswordButton.Text = isPasswordVisible ? "🙈" : "👁";
+
+    //    if (isPasswordVisible)
+    //    {
+    //        await Task.Delay(3000);
+
+    //        if (isPasswordVisible)
+    //        {
+    //            isPasswordVisible = false;
+    //            PasswordEntry.IsPassword = true;
+    //            TogglePasswordButton.Text = "👁";
+    //        }
+    //    }
+    //}
+
+    //SEGURAR PARA APARECER
+    private void OnPressed(object sender, EventArgs e)
+    {
+        PasswordEntry.IsPassword = false;
+    }
+
+    private void OnReleased(object sender, EventArgs e)
+    {
+        PasswordEntry.IsPassword = true;
+    }
+
+    async Task<bool> FakeLogin(string user, string senha)
+    {
+        await Task.Delay(2000);
+
+        return user == "admin" && senha == "123";
+    }
+
+    async Task Loading(int n)
+    {
+        if (n == 1)
+        {
+            LoadingIndicator.IsRunning = true;
+            LoadingIndicator.IsVisible = true;
+            LoginButton.IsEnabled = false;
+        }
+        else
+        {
+            LoadingIndicator.IsRunning = false;
+            LoadingIndicator.IsVisible = false;
+            LoginButton.IsEnabled = true;
+        }
+    }
+}
---
+++ MAUI/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnClicked(object? sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Login());
+        }
+    }
+}
